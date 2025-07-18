4b66e2707d631022c6c563465de4c0a50a4c419d
Evenilson Liandro <evenilsonlp@gmail.com>
2025-07-18 12:44:40 -0300

    feat(api): initialize API with PostgreSQL database and user authentication

--- apps/api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineAbilityFor, projectSchema } from '@saas/auth'
-
-const user = defineAbilityFor({ role: 'MEMBER', id: 'user-id' })
-
-const project = projectSchema.parse({ id: 'project-id', ownerId: 'user-id' })
-
-console.log(user.can('get', 'Billing'))
-console.log(user.can('create', 'Invite'))
-console.log(user.can('delete', project))
