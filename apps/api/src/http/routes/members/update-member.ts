abca914d8d0cb1e96bae7831bc48fc10b511a670
Evenilson Liandro <evenilsonlp@gmail.com>
2025-07-22 12:15:24 -0300

    feat(members): add update member endpoint with permission checks

---
+++ apps/api/src/http/routes/members/update-member.ts
@@ -0,0 +1,62 @@
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { BadRequestError } from '../_errors/bad-request-error'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+import { roleSchema } from '@saas/auth'
+
+export async function updateMember(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .put(
+      '/organizations/:slug/members/:memberId',
+      {
+        schema: {
+          tags: ['members'],
+          summary: 'Update a member',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+            memberId: z.uuid(),
+          }),
+          body: z.object({
+            role: roleSchema,
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug, memberId } = request.params
+        const userId = await request.getCurrentUserId()
+        const { membership, organization } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('update', 'User')) {
+          throw new BadRequestError(
+            'You do not have permission to update this member.',
+          )
+        }
+
+        const { role } = request.body
+
+        await prisma.member.update({
+          where: {
+            id: memberId,
+            organizationId: organization.id,
+          },
+          data: {
+            role,
+          },
+        })
+
+        return reply.status(204).send()
+      },
+    )
+}
