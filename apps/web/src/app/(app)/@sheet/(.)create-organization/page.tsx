be1f54ca812133f52ebfd2cfdf6a139b56966321
Evenilson Liandro <evenilsonlp@gmail.com>
2025-07-28 09:20:56 -0300

    feat(sheet): add InterceptedSheetContent component and update CreateOrganization page

--- apps/web/src/app/(app)/@sheet/(.)create-organization/page.tsx
+++ apps/web/src/app/(app)/@sheet/(.)create-organization/page.tsx
@@ -1,22 +1,18 @@
-import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-} from '@/components/ui/sheet'
+import { Sheet, SheetHeader, SheetTitle } from '@/components/ui/sheet'
 import { OrganizationForm } from '../../create-organization/organization-form'
+import { InterceptedSheetContent } from '@/components/intercepted-sheet-content'
 
 export default function CreateOrganization() {
   return (
     <Sheet defaultOpen>
-      <SheetContent>
+      <InterceptedSheetContent>
         <SheetHeader>
           <SheetTitle>Create organization</SheetTitle>
         </SheetHeader>
         <div className="px-2 py-4">
           <OrganizationForm />
         </div>
-      </SheetContent>
+      </InterceptedSheetContent>
     </Sheet>
   )
 }
