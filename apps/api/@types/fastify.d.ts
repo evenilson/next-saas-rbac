913b17f2a37ccac66b6b045d0d87c788701523c6
Evenilson Liandro <evenilsonlp@gmail.com>
2025-08-01 15:57:29 -0300

    fix: fix prisma import error

--- apps/api/@types/fastify.d.ts
+++ apps/api/@types/fastify.d.ts
@@ -1,6 +1,6 @@
 import 'fastify'
 
-import { Organization, Member } from '../src/generated/prisma'
+import { Organization, Member } from '@prisma/client'
 declare module 'fastify' {
   export interface FastifyRequest {
     getCurrentUserId(): Promise<string>
