ff5bf4e9d0909772d1e4ef9c350676a6269e8fac
Evenilson Liandro <evenilsonlp@gmail.com>
2025-08-01 00:53:04 -0300

    feat(invite): add create invite form and remove invite button

--- apps/web/src/app/(app)/org/[slug]/members/actions.ts
+++ apps/web/src/app/(app)/org/[slug]/members/actions.ts
@@ -1,10 +1,78 @@
 'use server'
 
 import { getCurrentOrg } from '@/auth/auth'
+import { createInvite } from '@/http/create-invite'
 import { removeMember } from '@/http/remove-member'
+import { revokeInvite } from '@/http/revoke-invite'
 import { updateMember } from '@/http/update-member'
-import { Role } from '@saas/auth'
+import { Role, roleSchema } from '@saas/auth'
+import { HTTPError } from 'ky'
 import { revalidateTag } from 'next/cache'
+import { z } from 'zod'
+
+const inviteSchema = z.object({
+  email: z.email({ message: 'Invalid e-mail address.' }),
+  role: roleSchema,
+})
+
+export async function creteInviteAction(data: FormData) {
+  const result = inviteSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const errors: Record<string, string[]> = {}
+
+    for (const issue of result.error.issues) {
+      const path = issue.path.join('.')
+
+      if (!path) continue
+
+      if (!errors[path]) {
+        errors[path] = []
+      }
+
+      errors[path].push(issue.message)
+    }
+
+    return {
+      success: false,
+      message: null,
+      errors,
+    }
+  }
+
+  const { email, role } = result.data
+
+  try {
+    const org = await getCurrentOrg()
+    await createInvite({
+      org: org!,
+      email,
+      role,
+    })
+
+    revalidateTag(`${org}/invites`)
+  } catch (error) {
+    if (error instanceof HTTPError) {
+      const { message } = await error.response.json()
+
+      return { success: false, message, errors: null }
+    }
+
+    console.error(error)
+
+    return {
+      success: false,
+      message: 'An unexpected error occurred. Please try again later.',
+      errors: null,
+    }
+  }
+
+  return {
+    success: true,
+    message: 'Successfully created the invite.',
+    errors: null,
+  }
+}
 
 export async function removeMemberAction(memberId: string) {
   const currentOrg = await getCurrentOrg()
@@ -27,4 +95,15 @@ export async function updateMemberAction(memberId: string, role: Role) {
   })
 
   revalidateTag(`${currentOrg}/members`)
+}
+
+export async function revokeInviteAction(inviteId: string) {
+  const currentOrg = await getCurrentOrg()
+
+  await revokeInvite({
+    org: currentOrg!,
+    inviteId,
+  })
+
+  revalidateTag(`${currentOrg}/invites`)
 }
